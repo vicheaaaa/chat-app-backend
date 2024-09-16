b9a136a2e5d1b23b18e52491299e0e5bf4595f73
vicheaaaa <123868063+vicheaaaa@users.noreply.github.com>
2024-09-16 07:58:52 +0700

    Update connectToMongoDB.js

--- db/connectToMongoDB.js
+++ db/connectToMongoDB.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectToMongoDB = async () => {
+  mongoose.Promise = global.Promise;
   try {
     console.log('MONGO_DB_URI:', process.env.MONGO_DB_URI); // Add this for debugging
     await mongoose.connect(process.env.MONGO_DB_URI, {
