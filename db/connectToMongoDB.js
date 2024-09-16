673ddb6e12e40edd97333a482abd83d3c2f4493e
vicheaaaa <123868063+vicheaaaa@users.noreply.github.com>
2024-09-16 08:13:47 +0700

    Update connectToMongoDB.js

--- db/connectToMongoDB.js
+++ db/connectToMongoDB.js
@@ -1,17 +1,24 @@
 import mongoose from 'mongoose';
 
 const connectToMongoDB = async () => {
-  mongoose.Promise = global.Promise;
   try {
-    console.log('MONGO_DB_URI:', process.env.MONGO_DB_URI); // Add this for debugging
+    console.log('MONGO_DB_URI:', process.env.MONGO_DB_URI); // Debugging the MongoDB URI
     await mongoose.connect(process.env.MONGO_DB_URI, {
-      useNewUrlParser: true,
+      useNewUrlParser: true, // These options might not be necessary in newer Mongoose versions
       useUnifiedTopology: true,
     });
     console.log("Connected to MongoDB");
   } catch (error) {
-    console.log("Error connecting to DB", error);
+    console.error("Error connecting to MongoDB:", error.message, error.stack);
+    process.exit(1); // Exit the process if connection fails
   }
 };
+
+// Optional: Gracefully close the MongoDB connection when the application exits
+process.on('SIGINT', async () => {
+  await mongoose.connection.close();
+  console.log("MongoDB connection closed due to app termination");
+  process.exit(0);
+});
 
 export default connectToMongoDB;
